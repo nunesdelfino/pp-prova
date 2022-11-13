ed20e8f083f45870c9d9a48361269df157a50bb9
nunesdelfino <gabrielnunesdelfinoo@ourlook.com>
2022-11-13 13:38:17 -0300

    desafio04

--- desafio4.php
+++ desafio4.php
@@ -6,122 +6,121 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>PROBLEMA 4</title>
 
-        <script src="https://kit.fontawesome.com/7f335cf7b9.js" crossorigin="anonymous" ></script>
-
+        <script src="https://kit.fontawesome.com/7f335cf7b9.js" crossorigin="anonymous"></script>
 
         <link
             rel="stylesheet"
             href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css"
             integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T"
             crossorigin="anonymous">
-            <link rel="stylesheet" href="style.css">
+        <link rel="stylesheet" href="style.css">
     </head>
     <body>
 
-    <!-- NAVBAR -->
-    <?php include ('navegacao.php');?>
+        <!-- NAVBAR -->
+        <?php include ('navegacao.php');?>
 
-    <main>
-        <div class="titulo">
+        <main>
+            <div class="titulo">
                 <h3 >
-                    PROBLEMA (4) - 
+                    PROBLEMA (4) -
                 </h3>
-        </div>
-        <div class="container">
-            <form action="ativ_formulario2.php" method="get">
-            <div class="form-group">
-                            <label>Nome</label>
-                            <input
-                                type="text"
-                                class="form-control"
-                                name="nome"
-                                placeholder="Insira o nome" required>
             </div>
-            <div class="form-group">
-                            <label>Idade</label>
-                            <input
-                                type="number"
-                                class="form-control"
-                                name="number"
-                                placeholder="Insira a idade" required>
-            </div>
-            
-<div>
-<label>SEXO</label><br>
-            <div class="form-check form-check-inline">
-           
+            <div class="container">
+                <form action="ativ_formulario2.php" method="get">
+                    <div class="form-group">
+                        <label>Nome</label>
+                        <input
+                            type="text"
+                            class="form-control"
+                            name="nome"
+                            placeholder="Insira o nome"
+                            required="required">
+                    </div>
+                    <div class="form-group">
+                        <label>Idade</label>
+                        <input
+                            type="number"
+                            class="form-control"
+                            name="idade"
+                            placeholder="Insira a idade"
+                            required="required">
+                    </div>
+
+                    <div>
+                        <label>SEXO</label><br>
+                        <div class="form-check form-check-inline">
+
                             <input
                                 class="form-check-input"
                                 type="radio"
-                                name="operacao"
-                                value="masc"
+                                name="sexo"
+                                value="Masculino"
                                 checked="checked">
                             <label class="form-check-label">
                                 MASCULINO
                             </label>
                         </div>
 
-                    <div class="form-check form-check-inline">
-                        <input class="form-check-input" type="radio" name="operacao" value="femin">
-                        <label class="form-check-label">
-                            FEMININO
-                        </label>
+                        <div class="form-check form-check-inline">
+                            <input class="form-check-input" type="radio" name="sexo" value="Feminino">
+                            <label class="form-check-label">
+                                FEMININO
+                            </label>
+                        </div>
                     </div>
-                </div>
 
-                <div>
-                    <br><label>Selecione os dispositivo que você possui</label><br>
-                    <div class="form-check">
-                        <input class="form-check-input" type="checkbox" value="pc" id="defaultCheck1">
-                        <label class="form-check-label" >
-                            Computador
-                        </label>
+                    <div>
+                        <br>
+                        <label>Selecione os dispositivo que você possui</label><br>
+                        <div class="form-check">
+                            <input class="form-check-input" type="checkbox" name="pc" value="pc" id="defaultCheck1">
+                            <label class="form-check-label">
+                                Computador
+                            </label>
                         </div>
                         <div class="form-check">
-                        <input class="form-check-input" type="checkbox" value="note" id="defaultCheck1">
-                        <label class="form-check-label">
-                            Notebook
-                        </label>
+                            <input class="form-check-input" type="checkbox" name="note" value="note" id="defaultCheck1">
+                            <label class="form-check-label">
+                                Notebook
+                            </label>
                         </div>
                         <div class="form-check">
-                        <input class="form-check-input" type="checkbox" value="cel" id="defaultCheck1">
-                        <label class="form-check-label" >
-                            Smartphone
-                        </label>
+                            <input class="form-check-input" type="checkbox" name="cel" value="cel" id="defaultCheck1">
+                            <label class="form-check-label">
+                                Smartphone
+                            </label>
                         </div>
-                </div>
-                <div>
-                
-                <br><label for="experiencia">Experiencia com programação:</label>
+                    </div>
+                    <div>
 
-                    <select name="experiencia" id="experiencia">
-                    <option value="nao-tem">Não Possui</option>
-                    <option value="Iniciante">Iniciante</option>
-                    <option value="Interm">Intermediário</option>
-                    <option value="avanc">Avançado</option>
-                    </select>
-                </div>
+                        <br>
+                        <label for="experiencia">Experiencia com programação:</label>
 
-                <button type="submit" class="btn btn-primary">Enviar</button>
-            
+                        <select name="experiencia" id="experiencia">
+                            <option value="nao-tem">Não Possui</option>
+                            <option value="Iniciante">Iniciante</option>
+                            <option value="Interm">Intermediário</option>
+                            <option value="avanc">Avançado</option>
+                        </select>
+                    </div>
 
-            </form> 
-        </div>
-    </main>
-    <script
-        src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
-        integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
-        crossorigin="anonymous">
-    </script>
-    <script
-        src="https://cdn.jsdelivr.net/npm/popper.js@1.14.7/dist/umd/popper.min.js"
-        integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1"
-        crossorigin="anonymous">
-    </script>
-    <script
-        src="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/js/bootstrap.min.js"
-        integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM"
-        crossorigin="anonymous">
-    </script>
+                    <button type="submit" class="btn btn-primary">Enviar</button>
+
+                </form>
+            </div>
+        </main>
+        <script
+            src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
+            integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
+            crossorigin="anonymous"></script>
+        <script
+            src="https://cdn.jsdelivr.net/npm/popper.js@1.14.7/dist/umd/popper.min.js"
+            integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1"
+            crossorigin="anonymous"></script>
+        <script
+            src="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/js/bootstrap.min.js"
+            integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM"
+            crossorigin="anonymous"></script>
     </body>
-    </html>
+</html>
